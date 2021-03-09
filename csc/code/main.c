5f8c87ffcfbe34971b2d5f45c44082a2d26db9d0
19375341 <19375341@stu-117.(none)>
2021-03-09 14:42:57 +0800

    fib_update

--- csc/code/main.c
+++ csc/code/main.c
@@ -3,9 +3,13 @@
 int main()
 {
 	int n,i;
+	int fnow = 1;
+	int fpre = 0;
 	scanf("%d",&n);
 	for(i=1;i<=n;i++){
-		printf("%d ",fibo(i));
+		fpre = fnow;
+		fnow = fnow + fpre;
 	}
+	prinf("%d", fnow);
 	return 0;
 }
