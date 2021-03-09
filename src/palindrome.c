c6efb200818dbd7128a2594ff60fda3cb98448dd
19375341 <19375341@stu-117.(none)>
2021-03-09 15:32:13 +0800

    fixpalindrome

--- src/palindrome.c
+++ src/palindrome.c
@@ -3,18 +3,13 @@ int main()
 {
 	int n;
 	scanf("%d",&n);
-
-
-
-
-
-
-
-
-
-
-
-	if(){
+	int rev = 0;
+	int b = n;
+	while(b!=0) {
+		rev  = rev * 10 + b % 10;
+		b = b / 10;
+	}
+	if(rev == n){
 		printf("Y");
 	}else{
 		printf("N");
