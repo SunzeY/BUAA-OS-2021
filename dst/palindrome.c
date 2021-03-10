a7aa234f25047e848c766acdedafe325d741d3c5
SunzeY <19375341@stu-117.(none)>
2021-03-10 16:47:41 +0800

    test

---
+++ dst/palindrome.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int rev = 0;
+	int  b = n;
+	while(b!=0) {
+		rev  = rev * 10 + b % 10;
+		b = b / 10;
+	}
+	if(rev == n){
+		printf("N");
+		printf("Y");
+	}else{
+		printf("N");
+	}
+	return 0;
+}
