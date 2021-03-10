10eef649f67a8eb32776f7e409f33778836fe2d7
SunzeY <19375341@stu-117.(none)>
2021-03-10 19:31:58 +0800

    fix

--- ray/sh_test1/changefile.sh
+++ ray/sh_test1/changefile.sh
@@ -2,12 +2,12 @@
 a=1
 while [ $a -le 100 ]
 do
-	if [ $a -gt 70 ]	   #if loop variable is greater than 70
+	if [ $a -gt 70 ];	   #if loop variable is greater than 70
 	then
-
-	elif [ $a -gt 40 ]         # else if loop variable is great than 40   
+		rm -rf file$a
+	elif [ $a -gt 40 ];         # else if loop variable is great than 40   		
 	then
-	
+		mv file$a newfile$a
 	fi
-	           #don't forget change the loop variable
+	let a=$a+1           #don't forget change the loop variable
 done
