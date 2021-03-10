e61023565c39f2695769574f2a37acf8ee503c8e
SunzeY <19375341@stu-117.(none)>
2021-03-10 15:33:01 +0800

    change palindrome

--- src/palindrome.c
+++ src/palindrome.c
@@ -4,12 +4,13 @@ int main()
 	int n;
 	scanf("%d",&n);
 	int rev = 0;
-	int b = n;
+	int  b = n;
 	while(b!=0) {
 		rev  = rev * 10 + b % 10;
 		b = b / 10;
 	}
 	if(rev == n){
+		printf("N");
 		printf("Y");
 	}else{
 		printf("N");
