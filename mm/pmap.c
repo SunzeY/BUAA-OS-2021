c9c197ee30786bc8b3ff24511a0f217c41f41eea
SunzeY <19375341@stu-117.(none)>
2021-04-06 15:49:45 +0800

    m11

--- mm/pmap.c
+++ mm/pmap.c
@@ -257,7 +257,7 @@ page_alloc2(struct Page **pp)
 	return 0;
 }
 
-int testtimes;
+int testtimes=1;
 void get_page_status(int pa)
 {	
 	int temp = 0;
@@ -282,7 +282,6 @@ void get_page_status(int pa)
 	else {
 		status = 3;
 	}
-	testtimes += 1;
 	printf("times:%d, page status:%d\n", testtimes, status);
 }
 
