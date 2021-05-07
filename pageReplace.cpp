a890b96987d8f1b5f807d7e3de5c2b438e75acaa
SunzeY <19375341@stu-117.(none)>
2021-05-07 23:19:02 +0800

    second chance

---
+++ pageReplace.cpp
@@ -0,0 +1,33 @@
+#include "pageReplace.h"
+#define MAX_PHY_PAGE 64
+#define NUM_OF_PAGE 524288
+#define MAX_PAGE 12
+#define GET_PAGE(x) ((x)>>MAX_PAGE)
+
+bool lastuse[MAX_PHY_PAGE];
+long map[NUM_OF_PAGE];
+
+//second chance:
+void pageReplace(long* physic_memory, long nwAdd)
+{
+    static int point = 0;
+    static int cur_filled_num = 0;
+    unsigned long page_num = get_Page(nwAdd);
+    if (map[page_num]>0) {
+        lastuse[map[page_num]] = true;
+    }
+    if (cur_filled_num<MAX_PHY_PAGE) {
+        map[page_num] = cur_filled_num;
+        physic_memory[cur_filled_num++] = page_num;
+    }
+    else {
+        while(lastuse[point]) {
+            lastuse[point] = false;
+            point = (point+1)&(MAX_PHY_PAGE-1);
+        }
+        map[lastuse[point]] = 0;
+        map[page_num] = point;
+        phsic_memory[point] = point;
+        point = (point+1) & (MAX_PHY_PAGE-1);
+    }
+}
