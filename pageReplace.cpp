9c32811a23c002a088c3269c15f57e5d80b86a2a
SunzeY <19375341@stu-117.(none)>
2021-05-08 09:54:26 +0800

    mem_opt

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -1,13 +1,12 @@
 #pragma GCC optimize (2)
 #include "pageReplace.h"
 #define MAX_PHY_PAGE 64
-#define NUM_OF_PAGE 524288
 #define MAX_PAGE 12
 #define true 1
 #define false 0
 #define GET_PAGE(x) ((x)>>MAX_PAGE)
 
-char lastuse[MAX_PHY_PAGE];
+/*char lastuse[MAX_PHY_PAGE];
 char map[NUM_OF_PAGE];
 
 //second chance:
@@ -35,3 +34,33 @@ void pageReplace(long* physic_memory, long nwAdd)
         point = (point+1) & (MAX_PHY_PAGE-1);
     }
 }
+*/
+
+char lastuse[MAX_PHY_PAGE];
+void pageReplace(long* physic_memory, long nwAdd)
+{
+    static char point = 0;
+    static char cur_filled_num = 0;
+    long page_num = GET_PAGE(nwAdd);
+    for (point = 0; point <MAX_PHY_PAGE; point++){
+        if (page_num==physic_memory[point]) {
+            lastuse[point] = true;
+            return;
+        }
+    }
+    point = 0;
+    if (cur_filled_num<MAX_PHY_PAGE) {
+        physic_memory[cur_filled_num++] = page_num;
+    }
+    else {
+        while(lastuse[point]) {
+            lastuse[point] = false;
+            point = (point+1) & (MAX_PHY_PAGE-1);
+        }
+        physic_memory[point] = page_num;
+        point = (point+1) & (MAX_PHY_PAGE-1);
+    }
+}
+
+
+
