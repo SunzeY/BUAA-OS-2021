4de8cd59d1ec691ee66bd7ce09289e9d6680697a
10.210.0.67 <10.210.0.67@example.com>
2021-05-11 14:36:56 +0800

    Judgement for lab4-1-exam at 2021-05-11T14:36:56+0800

--- user/mytest.c
+++
@@ -1,9 +0,0 @@
-#include "lib.h"
-void umain(){
-    writef(">>>>>>mytest_here:\n");
-    writef("hello world!\n");
-    syscall_putchar('c');
-    syscall_putchar('d');
-    writef("cur_envid is %d\n", syscall_getenvid);
-
-}
