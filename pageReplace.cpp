a5302663dfd0ac3467e11ba41c2450bf1d3ea685
SunzeY <19375341@stu-117.(none)>
2021-05-12 19:06:16 +0800

    test

--- pageReplace.cpp
+++ pageReplace.cpp
@@ -100,16 +100,16 @@ void pageReplace(long* physic_memory, long nwAdd)
 
 //secont_chance_opt_search
 
-char lastuse[MAX_PHY_PAGE];
+int lastuse[MAX_PHY_PAGE];
 
 void pageReplace(long* physic_memory, long nwAdd)
 {
     static char point = 0;
     static char last_point = 0;
     static char cur_filled_num = 0;
-    int page_num = GET_PAGE(nwAdd);
-    char temp_point = 0;
-    for (char i = 0; i <=MAX_PHY_PAGE_HALF; i++){
+    register int page_num = GET_PAGE(nwAdd);
+    register int temp_point = 0;
+    for (register int i = 0; i <=MAX_PHY_PAGE_HALF; i++){
         temp_point = (i+last_point)&(MAX_PHY_PAGE-1);
         if (page_num==physic_memory[temp_point]) {
             lastuse[temp_point]=true;
