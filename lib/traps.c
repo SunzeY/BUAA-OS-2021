a8c549f90fe214fe4adee71ae3406863aa14c392
SunzeY <19375341@stu-117.(none)>
2021-05-18 16:03:28 +0800

    a

--- lib/traps.c
+++ lib/traps.c
@@ -55,9 +55,9 @@ page_fault_handler(struct Trapframe *tf)
     extern struct Env *curenv;
 
     bcopy(tf, &PgTrapFrame, sizeof(struct Trapframe));
-    int* instr = tf->pc;
+    int* instr = tf->cp0_epc;
     curenv->env_pgcow++;
-    printf("\nEnv:0x%x, code:0x%x, pgcow:%d, pgout:%d\n", curenv->env_id, *instr, curenv->env_pgout, curenv->env_pgout);
+    printf("\nEnv:0x%x, code:0x%x, pgcow:%d, pgout:%d\n", curenv->env_id, *instr, curenv->env_pgcow, curenv->env_pgout);
     if (tf->regs[29] >= (curenv->env_xstacktop - BY2PG) &&
         tf->regs[29] <= (curenv->env_xstacktop - 1)) {
             tf->regs[29] = tf->regs[29] - sizeof(struct  Trapframe);
