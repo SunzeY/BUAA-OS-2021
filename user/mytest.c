bf4583dbc2c8048b5bf9ceda1ec939f4f5781f41
SunzeY <19375341@stu-117.(none)>
2021-05-18 15:28:35 +0800

    a

--- user/mytest.c
+++ user/mytest.c
@@ -1,9 +1,19 @@
 #include "lib.h"
 int c;
 int d = 0;
+int global_a;
 void umain(){
     int b = 1;
     int f = 1;
-    int a = tfork();
+    int id = 0;
+    if (id = tfork()==0) {
+        global_a+=3;
+        writef("\n child : %d\n", global_a);
+    }
+    else {
+        global_a+=5;
+        writef("\n father : %d\n", global_a);
+    }
+    
     for (;;);
 }
