d90f87ef99db4d9f4573f4d4a75cc1ce6ab09133
SunzeY <19375341@stu-117.(none)>
2021-06-07 16:56:58 +0800

    a

--- fs/fsformat.c
+++ fs/fsformat.c
@@ -205,10 +205,10 @@ int make_link_block(struct File *dirf, int nblk) {
 //      use make_link_block function
 struct File *create_file(struct File *dirf) {
     struct File *dirblk;
-    int i, bno, found;
+    int i, j, bno, found;
     int nblk = dirf->f_size / BY2BLK;
     
-    // Your code here
+    /*// Your code here
     // Step1: According to different range of nblk, make classified discussion to 
     if (nblk == 0) {
         return (struct File*)(disk[make_link_block(dirf, 0)].data);
@@ -225,6 +225,19 @@ struct File *create_file(struct File *dirf) {
     for (i=0; i<FILE2BLK; ++i) {
         if (dirblk[i].f_name[0]=='\0') {
             return &dirblk[i];
+        }
+    }*/
+    for (i=0; i< nblk; i++) {
+        if (i<NDIRECT) {
+            bno = dirf->f_direct[i];
+        } else {
+            bno = ((int*)disk[dirf->f_indirect].data)[i];
+        }
+        dirblk = (struct File*) (disk[bno].data);
+        for (j=0; j<FILE2BLK; j++) {
+            if (dirblk[j].f_name[0] == '\0') {
+                return &dirblk[j];
+            }
         }
     }
     // Step3(self): no free file link, make a new one.
