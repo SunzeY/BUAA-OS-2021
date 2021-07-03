a8505b34f28dc9f29d5022ef6025bc19456e1e59
SunzeY <19375341@stu-117.(none)>
2021-07-03 21:10:52 +0800

    a better tree.b

--- user/tree.c
+++ user/tree.c
@@ -7,6 +7,7 @@ void
 treePath(char* path, int offset) {
     int fd, n;
     struct File f;
+    struct File fnext;
     struct File lastf;
     if ((fd = open(path, O_RDONLY)) < 0) {
         PRINT_FONT_RED
@@ -22,23 +23,33 @@ treePath(char* path, int offset) {
     }
     PRINT_ATTR_REC
     char* tempath[1024];
-    while ((n = readn(fd, &f, sizeof f))==sizeof f) {
+   
+    if ((n = readn(fd, &fnext, sizeof f))!=sizeof f) {
+       close(fd);
+       return;
+    }
+    int end = 0;
+    while ( end!=1 ) {
+        f = fnext;
+        if ((n = readn(fd, &fnext, sizeof f))!=sizeof f || fnext.f_name[0] == 0) {
+            end = 1;
+        }
         if (f.f_name[0]!= 0 && f.f_type == FTYPE_DIR){
             for(m=0; m<offset; m++) {
                 if (m%5 == 0) {
-                    writef(" ");
+                    writef("│");
                 } else {
                     writef(" ");
                 }
-            }   
-            writef("├──");
+            }
+            if (end) writef("└──");
+            else writef("├──");
             PRINT_FONT_CYA
             writef("%s\n\033[1A", f.f_name);
             PRINT_ATTR_REC
             int k = 0;
             char tempath[1024];
             for(k=0; k<1024; k++) tempath[k] = '\0';
-            // writef("combine %s + %s\n", path, f.f_name);
             strcpy(tempath, path);
             strcpy((tempath + strlen(tempath)), "/");
             strcpy((tempath + strlen(tempath)), f.f_name);
@@ -46,12 +57,13 @@ treePath(char* path, int offset) {
         } else if (f.f_name[0] != 0 ){
              for(m=0; m<offset; m++) {
                 if (m%5 == 0) {
-                    writef(" ");
+                    writef("│");
                 } else {
                     writef(" ");
                 }
              }   
-             writef("├──%s\n\033[1A", f.f_name);
+             if (end) writef("└──%s\n\033[1A", f.f_name);
+             else writef("├──%s\n\033[1A", f.f_name);
         }
     }
     close(fd);
@@ -59,8 +71,6 @@ treePath(char* path, int offset) {
 
 void
 umain(int argvn, char** argv) {
-    //writef(">>>>%d\n", argvn);
-    //writef(">>>>%s\n", argv[1]);
     if (argvn == 2 && strcmp(argv[1], "-a") == 0) {
         treePath("/", 0);
     }
